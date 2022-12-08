d785c99360d758089b6bc706620271398cb8a5a2
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-08 10:32:01 +0800

    Modify query 1 and create query 3

--- OnlineInventorySystem/select.sql
+++ OnlineInventorySystem/select.sql
@@ -1,11 +1,19 @@
+ -- Query 1
 SELECT
   Orders.Order_ID,
   Orders.ProductName,
-  Payment.Payment_ID
+  Customers.Customer_ID,
+  Customers.CustomerFirstName + ' ' + Customers.CustomerLastName AS CustomerName,
+  Orders.OrderQuantity * Orders.OrderTotal AS TotalAmount,
+  Payment.Payment_ID,
+  Payment.PaymentType
 FROM
   Orders
-  LEFT JOIN Payment ON Orders.Payment_ID = Payment.Payment_ID;
+  LEFT JOIN Customers ON Orders.Customer_ID = Customers.Customer_ID
+  LEFT JOIN Payment ON Orders.Payment_ID = Payment.Payment_ID
+  AND Orders.Order_ID = Payment.Order_ID;
 
+-- Query 2
 SELECT
   Products.ProductName,
   Products.ProductPrice,
@@ -13,3 +21,9 @@ SELECT
 FROM
   Products
   LEFT JOIN Suppliers ON Products.Supplier_ID = Suppliers.Supplier_ID;
+
+-- Query 3
+SELECT
+  SUM(OrderQuantity * OrderTotal) AS TotalSales
+FROM
+  Orders;
