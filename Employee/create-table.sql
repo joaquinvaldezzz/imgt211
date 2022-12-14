5bc7fa5db9eecedcb15e36cabe1ba84efcfc8ce0
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-14 21:22:51 +0800

    Add semicolons

--- Employee/create-table.sql
+++ Employee/create-table.sql
@@ -10,17 +10,20 @@ CREATE TABLE
     region VARCHAR(256),
     postal_code VARCHAR(256),
     country VARCHAR(256)
-  )
+  );
+
 CREATE TABLE
-  Region (region_id VARCHAR(256) PRIMARY KEY, region_description VARCHAR(256))
+  Region (region_id VARCHAR(256) PRIMARY KEY, region_description VARCHAR(256));
+
 CREATE TABLE
   Territories (
     territory_id INT PRIMARY KEY,
     territory_description VARCHAR(256),
     region_id VARCHAR(256) FOREIGN KEY REFERENCES Region (region_id)
-  )
+  );
+
 CREATE TABLE
   EmployeeTerritories (
     employee_id INT FOREIGN KEY REFERENCES Employees (employee_id),
     territory_id INT FOREIGN KEY REFERENCES Territories (territory_id),
-  )
+  );
