03cdb44a4c781afcfb4fce5ac18764f88e48b275
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-14 21:22:32 +0800

    Delete SELECT queries

--- Employee/insert-data.sql
+++ Employee/insert-data.sql
@@ -10,40 +10,27 @@ VALUES
     'Region 3',
     '3023',
     'Philippines'
-  )
-SELECT
-  *
-FROM
-  Employees
+  );
+
 INSERT INTO
   Region
 VALUES
-  (001, 'Region 3')
-SELECT
-  *
-FROM
-  Region
+  (001, 'Region 3');
+
 INSERT INTO
   Territories
 VALUES
-  (01, 'San Jose Del Monte', 001)
-SELECT
-  *
-FROM
-  Territories
+  (01, 'San Jose Del Monte', 001);
+
 INSERT INTO
   EmployeeTerritories
 VALUES
-  (1, 01)
-SELECT
-  *
-FROM
-  EmployeeTerritories
-  ---
+  (1, 01);
+
 SELECT
   region
 FROM
   Territories AS T
   INNER JOIN Employees AS E ON T.region_id = E.employee_id
   INNER JOIN EmployeeTerritories AS ET ON T.territory_id = T.territory_id
-  INNER JOIN Region AS R ON R.region_id = T.region_id
+  INNER JOIN Region AS R ON R.region_id = T.region_id;
